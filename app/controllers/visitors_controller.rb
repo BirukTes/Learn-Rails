8da47dc707748e252cf715419da92afbd91d9b1a
biruktes <biruktes@outlook.com>
2017-11-03 20:11:26 +0000

    Some changes

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,7 @@
 class VisitorsController < ApplicationController
   def new
+    # Rails.logger.debug 'DEBUG: entering new method'
+    # raise 'Deliberate Failure'
     @owner = Owner.new
     render 'visitors/new'
   end

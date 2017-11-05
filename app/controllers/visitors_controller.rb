38c1754a5662aa43803b70bf510c962eaafcb2b1
biruktes <biruktes@outlook.com>
2017-11-05 20:34:18 +0000

    update application layout, pushing via vs code

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,19 @@ class VisitorsController < ApplicationController
     # Rails.logger.debug 'DEBUG: entering new method'
     # raise 'Deliberate Failure'
     @owner = Owner.new
-    render 'visitors/new'
+    # There is no need for this line as Flash
+    # render 'visitors/new'
+
+    # Flash allow to display a message temporarily, its a hash object in Ruby
+    # We can assign other messages, such as flash[:alert] or even
+    # flash[:warning]. In practice, Rails uses only :notice and :alert as flash message keys so it is
+    # wise to stick with just these.
+    # Assign the value to the key (flash[:key]=value), names are arbitrary but make them meaningful as follows
+    flash[:notice] = 'Welcome!'
+    flash[:alter] = 'My birthday is soon.'
+    flash.each do |key, value|
+      # Use key as the is class name and value as the
+      puts '<div class="' + key + '">' + value + '</div>'
+    end
   end
 end
